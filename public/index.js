b4f809cc05d3fe700b6a71e62fd61fbf93ec19b3
Jacob Varns <jacob.varns@gmail.com>
2020-11-10 20:12:09 -0800

    feat: separated votes into component parts. added copy button

--- public/index.js
+++ public/index.js
@@ -2,11 +2,29 @@ function sendSelection(elem, severity, frequency) {
   var userId = window.userId;
   var roomId = window.roomId;
 
-  if ($(elem).hasClass("selected")) {
-    window.socket.emit("unvote", { userId, roomId });
-  } else {
-    window.socket.emit("vote", { severity, frequency, userId, roomId });
+  var myVote = null;
+
+  for (let i = 0; i < window.votes.length; i++) {
+    if (window.votes[i].userId === userId) {
+      myVote = window.votes[i];
+    }
   }
+
+  if (!myVote) {
+    myVote = { severity, frequency, userId, roomId };
+  } else if (severity === null && frequency === myVote.frequency) {
+    myVote.frequency = null;
+  } else if (severity === myVote.severity && frequency === null) {
+    myVote.severity = null;
+  } else {
+    if (severity !== null) {
+      myVote.severity = severity;
+    }
+    if (frequency !== null) {
+      myVote.frequency = frequency;
+    }
+  }
+  window.socket.emit("vote", myVote);
 }
 
 function clearSelections(severity, frequency) {
@@ -14,6 +32,23 @@ function clearSelections(severity, frequency) {
     var userId = window.userId;
     var roomId = window.roomId;
     window.socket.emit("clear", { userId, roomId });
+  }
+}
+
+function copyResults() {
+  if (!navigator.clipboard) {
+    // Clipboard API not available
+    return;
+  }
+  try {
+    var text = "Frequency:\n";
+    text = text + $("#results-frequency h3").text();
+    text = text + "\nSeverity:\n";
+    text = text + $("#results-severity h3").text();
+
+    navigator.clipboard.writeText(text);
+  } catch (err) {
+    console.error("Failed to copy!", err);
   }
 }
 
@@ -52,23 +87,36 @@ $(function () {
   window.socket = socket;
 
   socket.on("vote", function (votes) {
-    $("#results h3").text("");
+    $("#results-frequency h3").text("");
+    $("#results-severity h3").text("");
     $(".selected").removeClass("selected");
+    window.votes = votes;
 
     if (votes && votes.length > 0) {
-      var msgs = [];
+      var freqs = [];
+      var sevs = [];
       var userId = window.userId;
       for (let i = 0; i < votes.length; i++) {
-        msgs.push(votes[i].severity + votes[i].frequency);
+        if (votes[i].severity !== null) {
+          sevs.push(votes[i].severity);
+        }
+        if (votes[i].frequency !== null) {
+          freqs.push(votes[i].frequency);
+        }
 
         if (votes[i].userId === userId) {
           $(`.r${votes[i].severity} .c${votes[i].frequency}`).addClass(
             "selected"
           );
+
+          $(`.S${votes[i].severity}`).addClass("selected");
+          $(`.F${votes[i].frequency}`).addClass("selected");
         }
       }
-      msgs = msgs.sort();
-      $("#results h3").text(msgs.join(", "));
+      freqs = freqs.sort();
+      sevs = sevs.sort();
+      $("#results-frequency h3").text(freqs.join(", "));
+      $("#results-severity h3").text(sevs.join(", "));
     }
   });
 });
